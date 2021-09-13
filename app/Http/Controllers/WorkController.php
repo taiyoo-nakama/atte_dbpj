471adc4fcdc1fe0e8713d145e4bd584fa9be18ce
仲間太陽 <taiyoonakama@gmail.com>
2021-09-13 15:14:00 +0900

    count->error

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -13,11 +13,14 @@ use Carbon\Carbon;
 class WorkController extends Controller
 {
     //Work model
-    public function work(){
+    public function work(Request $request){
        /* $user = Work::user();
         $items = Work::paginate(4);
         $param = ['items' =>$items,'user'=>$user];*/
-    return view('tests.work-test1',/*$param*/);
+        $date = Carbon::today();
+    return view('tests.work-test1',[
+        'date' => $date,
+    ]/*$param*/);
     }
     public function attendance(){
         return view('tests.attendance-test1');
@@ -26,29 +29,16 @@ class WorkController extends Controller
     //勤務開始
     public function start(Request $request)
     {
-        //打刻1日一回
-        /*$oldTimestamp = Work::where('user_id',$request->user_id)->latest()->first();
-        if($oldTimestamp){
-            $oldTimestamp_start = new Carbon($oldTimestamp->start);
-            $oldTimestampDay = $oldTimestamp_start->startOfDay();
-        }
-
-        $newTimestampDay = Carbon::today();
-
-        //日付比較。同日月の出勤打刻で、退勤打刻がない場合エラーを吐き出す
-        if(($oldTimestampDay == $newTimestampDay) && (empty($oldTimestamp->end))){
-            return view('tests.work-test1');
-        }*/
-
         //createメソッド
         $timestamp = Work::create([
             'user_id' => 1,
-            $date = Carbon::today(),
             $start_times = Carbon::now(),
+            //'date' => '',
+            //$date = Carbon::today(),
         ]);
         return view('tests.work-test1',[
-            'start_time' => $start_times,
-            'date' => $date,
+            'start_times' => $start_times,
+            //'date' => $date,
         ]);
     }
     //勤務終了
